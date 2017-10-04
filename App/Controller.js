9196f5d55ad3f2a3d41f97ab9e9515cdf4c28462
DoCuong-Company <gacdemxuan@gmail.com>
2017-10-04 18:04:39 +0700

    Commit by DoCuong-Company
    201710041804

--- App/Controller.js
+++ App/Controller.js
@@ -1,26 +1,11 @@
 (function (app) {
     app.controller('MaterialController', function ($scope, materialService) {
-        $scope.materialService = materialService;
-        $scope.$watch('materialService.data', function () {
-            $scope.todoList = $scope.materialService.data;
-        });
-        $scope.materialService.findAll(function (data) {
-            $scope.todoList = data;
-            $scope.$apply();
-        });
-        $scope.add = function () {
-            materialService.add($scope.newContent);
-            $scope.newContent = '';
-        }
-        $scope.remove = function (todo) {
-            materialService.remove(todo);
-        }
-        $scope.removeAll = function () {
-            materialService.removeAll();
-        }
-        $scope.toggleCompleted = function () {
-            materialService.sync();
-        }
+
+        $scope.entity = { "value1": 1, "value2": 1, "value3": 1 };
+        $scope.total = 1;
+        $scope.changeValue = function (val) {
+            $scope.total = $scope.entity.value1 * $scope.entity.value2 * $scope.entity.value3;
+        };
     });
 
 }(angular.module("Application")));
